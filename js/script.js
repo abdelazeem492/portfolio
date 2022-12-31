f61689e4794978e7facbe68e8d3034c309bc7054
abdelazeem492 <111613438+abdelazeem492@users.noreply.github.com>
2022-12-31 19:42:46 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-$(document).ready(function () {
-  $("#menu").click(function () {
-    $(this).removeClass("fa-solid fa-xmark");
+let menu = document.querySelector("#menu");
+let header = document.querySelector("header");
 
-    $("header").toggleClass("toggle");
-  });
+function toggle() {
+  header.classList.toggle("toggle");
+  $("#menu").toggleClass("fa-solid fa-xmark");
+  $("#menu").toggleClass("fas fa-bars");
+}
+
+$(document).ready(function () {
+  // $("#menu").click(function () {
+  //   // $("#menu").removeClass("fa-solid fa-xmark");
+
+  //   $("header").toggleClass("toggle");
+  // });
 
   $(window).on("scroll load", function () {
     $("#menu").toggleClass("fas fa-bars");
